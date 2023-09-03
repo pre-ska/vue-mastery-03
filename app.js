a1e47f4711785e86deae48b2fe6eaf8d19551594
preska <wolfwar@outlook.com>
2023-09-03 16:37:44 +0200

    39 done

--- app.js
+++ app.js
@@ -7,7 +7,25 @@ const app = Vue.createApp({
 });
 
 app.component("todo-list-item", {
-  props: ["task"],
+  props: {
+    task: {
+      type: String,
+      required: true,
+      // validator(value) { // ! validator radi samo u dev enviromentu
+      //   return value.length > 0;
+      // }
+    },
+    id: {
+      type: Number,
+      required: false,
+      validator(value) {
+        return value >= 1 && value <= 100;
+      },
+      // default() { // ! mora biti funkcija za niz i obj, za primitive može "default: 5"
+      //   return []
+      // }
+    },
+  },
   template: `
   <div 
     class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4"
